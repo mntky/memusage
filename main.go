79a9a2ff0e5f966488994730f1681957b22c6429
mntky <djminatoo@gmail.com>
2019-08-01 09:57:54 +0900

    カーソル移動完全に理解した

--- main.go
+++ main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"os/exec"
+	"os"
 
 	"./info"
 )
 
+func clear() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
 func main() {
 	i := &info.Info{}
+	clear()
 
-	fmt.Println(i.MemTotal())
+	for {
+		fmt.Printf("MemTotal:%d",i.MemTotal())
+		fmt.Printf("\033[1E")
+		fmt.Printf("MemFree:%d",i.MemFree())
+		time.Sleep(1000*time.Millisecond)
+		clear()
+	}
 }
