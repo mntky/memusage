f0690f71b4f290bb7a155c76bdb441f1da1d0468
mntky <djminatoo@gmail.com>
2019-08-01 16:19:47 +0900

    add uptime() return day/hours/minutes/seconds

--- info/info.go
+++ info/info.go
@@ -17,8 +17,9 @@ func (i *Info) MemTotal() int {
 func (i *Info) MemFree() int {
 	return i.info(1)
 }
-func (i *Info) Available() int {
+func (i *Info) MemAvailable() int {
 	return i.info(2)
+}
 
 func (i *Info) info(num int) int {
 	var lines []int
@@ -34,8 +35,32 @@ func (i *Info) info(num int) int {
 		trim := strings.Trim(split[1], " ")
 		split = strings.Split(trim, " ")
 		i, _ := strconv.Atoi(split[0])
-		//fmt.Printf("%d	%s\n", i, "kB")
 		lines = append(lines, i)
 	}
 	return lines[num]
 }
+
+func (i *Info) Uptime() string {
+	var secondstr string
+	file, err := os.Open("/proc/uptime")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	line := bufio.NewScanner(file)
+	for line.Scan() {
+		secondstr = line.Text()
+	}
+	split := strings.Split(secondstr, " ")
+	second, _ := strconv.ParseFloat(split[0], 32)
+
+	upseconds := int(second)%60
+	upminutes := int(second)/60
+	uphours := upminutes/60
+	updays := uphours/24
+	uphours = uphours%24
+	ret := fmt.Sprintf("%d/%d/%d/%d",updays, uphours, upminutes, upseconds)
+	return ret
+}
+
